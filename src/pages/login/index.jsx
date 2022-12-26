e6ef9248e2ea26896ce55c9ea0898ef8570279a3
maryrrrr <m_rabiee85@yahoo.com>
2022-12-26 14:31:37 +0330

    feat: error message

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
-import Styles from '../login/Style.module.css';
-const Login =() =>{
-    const [email, setEmail] = useState(''); 
-    const [password, setPassword] = useState(''); 
-    const [Lmsg, setLMsg] = useState(''); 
-    const [user, setUser] = useState(''); 
-    const [session, setSession] = useState(''); 
-    return (
-    <>
-    <div style= {{fontSize:"2em", color:"#789c90", textAlign:"center"}}>
-          ورود به حساب کاربری
-      </div>
-      <div className={`${Styles.reg-form}`}>
-          نام کاربری:
-          <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder="لطفا نام کاربری را وارد کنید" /><br/>
-          رمز عبور:
-          <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="لطفا رمز عبور خود را وارد کنید" /><br/>
-    </div>
-    <div>
-      <button className={`${Styles.submit}`} onClick={Login}>ورود</button><br/>
-      </div>
-    <p>{Rmsg}</p>
+// import { useState } from "react";
+// import Styles from '../login/Style.module.css';
+// const Login =() =>{
+//     const [email, setEmail] = useState(''); 
+//     const [password, setPassword] = useState(''); 
+//     const [Lmsg, setLMsg] = useState(''); 
+//     const [user, setUser] = useState(''); 
+//     const [session, setSession] = useState(''); 
+//     return (
+//     <>
+//     <div style= {{fontSize:"2em", color:"#789c90", textAlign:"center"}}>
+//           ورود به حساب کاربری
+//       </div>
+//       <div className={`${Styles.reg-form}`}>
+//           نام کاربری:
+//           <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder="لطفا نام کاربری را وارد کنید" /><br/>
+//           رمز عبور:
+//           <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="لطفا رمز عبور خود را وارد کنید" /><br/>
+//     </div>
+//     <div>
+//       <button className={`${Styles.submit}`} onClick={Login}>ورود</button><br/>
+//       </div>
+//     <p>{Rmsg}</p>
 
-    </>
-    );
-}
-export default Login;
+//     </>
+//     );
+// }
+// export default Login;
--- src/pages/post/index.jsx
+++ src/pages/post/index.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
 
 const Post = () => {
-    const{id}= useParams();
+    const{postId}= useParams();
     const[title,setTitle]= useState([]);
     const[desc,setDesc]= useState([]);
     const[image,setImage]= useState([]);
+    const[comment, setComment]= useState([]);
+    const[like,setlike]= useState(false);
 
     useEffect( () => {
     
-        fetch( `https://frtpwlgschfrwsaawgiv.supabase.co/rest/v1/posts?id=eq.${id}&select=*`,{
+        fetch( `https://frtpwlgschfrwsaawgiv.supabase.co/rest/v1/posts?id=eq.${postId}&select=*`,{
             method: "GET",
             headers: {
             apikey: process.env.REACT_APP_SUPABASE_ANON_KEY ,
@@ -26,14 +28,57 @@ const Post = () => {
                }
             });
         }, []);
-    return(
+        const onFormSubmit= (e) =>{
+            e.preventDefault();
+            fetch('https://frtpwlgschfrwsaawgiv.supabase.co/rest/v1/comments',{
+                method: "POST",
+                headers: {
+                    apikey: process.env.REACT_APP_SUPABASE_ANON_KEY ,
+                    Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_ANON_KEY}`,
+                    "Content-Type": "application/json",
+                    "Prefer": "return=minimal",
+                },
+                body:JSON.stringify({comment:comment, username:"mary"})
+            })
+            .then((response)=> response.json())
+            .then((response) => {console.log(response);})
+            .catch((error)=> {console.log(error);
+            });
+        }
+
+        const getComment=(e) => {
+            setComment(e.target.value);
+        }
+
+    const LikeButton= ()=>{
+    useEffect( ()=>{
+        fetch('https://frtpwlgschfrwsaawgiv.supabase.co/rest/v1/likes',{
+            method:'POST',
+            header:  {
+                apikey: process.env.REACT_APP_SUPABASE_ANON_KEY ,
+                Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_ANON_KEY}`,
+                "Content-Type": "application/json",
+                "Prefer": "return=minimal",
+            },
+        })
+    }
+    ),[like];
+     return(
         <>
         <img src={image}/>
          <h1>{title}</h1>
          <p>{desc}</p>
+         <form onSubmit={onFormSubmit}>
+            <label>
+                توضیحات:
+                <textarea onChange={(e)=>getComment(e)} type="text" />
+            </label>
+            <button>submit</button>
+            <button onClick= {()=> setlike(prev=>!prev)}>{like?"Dislike":"Like"}</button>
+            </form>
         </>
     )
-    
+     }   
     
 }
 export default Post;
